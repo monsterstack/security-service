8c4807dfbd12897329ebe384f2a8d7e61dc1ccf8
zacharyrote <zachary.rote@gmail.com>
2017-02-06 19:33:55 -0500

    docker

---
+++ cluster.js
@@ -0,0 +1,29 @@
+'use strict';
+const config = require('config');
+const Cluster = require('core-server').Cluster;
+
+const optimist = require('optimist');
+
+const main = () => {
+  console.log("Starting Cluster");
+  let options = {};
+  if(optimist.argv.numWorkers) {
+    options.numWorkers = optimist.argv.numWorkers;
+  }
+
+  if(optimist.argv.discoveryHost) {
+    options.discoveryHost = optimist.argv.discoveryHost;
+  }
+
+  let announcement = require('./announcement.json');
+  let cluster = new Cluster(announcement.name, announcement, options);
+  //let exitHandler = proxy.exitHandlerFactory(cluster.id, model);
+  //cluster.bindExitHandler(exitHandler);
+
+  cluster.start();
+}
+
+
+if(require.main === module) {
+  main();
+}
