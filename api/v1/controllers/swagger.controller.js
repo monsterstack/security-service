8ce2fcb423f9e829221a23d77da1ac1ec47da83a
zacharyrote <zachary.rote@gmail.com>
2017-02-21 20:46:08 -0500

    security-service

--- api/v1/controllers/swagger.controller.js
+++ api/v1/controllers/swagger.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 const appRoot = require('app-root-path');
 const HttpStatus = require('http-status');
-const Error = require('core-server').ServiceError;
+const ServiceError = require('core-server').ServiceError;
 const SwaggerService = require('core-server').SwaggerService;
 const ip = require('ip');
 const swagger = require(appRoot + '/api/swagger/swagger.json');
@@ -10,7 +10,11 @@ const getSwagger = (app) => {
   return (req, res) => {
     let swaggerService = new SwaggerService('/api/v1');
     swaggerService.getSwagger().then((swagger) => {
-      res.status(HttpStatus.OK).send(swagger);
+      if(swagger === null) {
+        new ServiceError(HttpStatus.NOT_FOUND, 'Swagger Not Found').writeResponse(res);
+      } else {
+        res.status(HttpStatus.OK).send(swagger);
+      }
     }).catch((err) => {
       new ServiceError(HttpStatus.INTERNAL_SERVER_ERROR, err.message).writeResponse(res);
     });
