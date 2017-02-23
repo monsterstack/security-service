f8d68455a442cc264f75d72a7886dbe94441fd43
zacharyrote <zachary.rote@gmail.com>
2017-02-23 10:00:17 -0500

    security-service

--- api/v1/controllers/swagger.controller.js
+++ api/v1/controllers/swagger.controller.js
@@ -3,11 +3,13 @@ const appRoot = require('app-root-path');
 const HttpStatus = require('http-status');
 const ServiceError = require('core-server').ServiceError;
 const SwaggerService = require('core-server').SwaggerService;
+const config = require('config');
 
 const getSwagger = (app) => {
   return (req, res) => {
     let baseSwagger = require(appRoot + '/api/swagger/swagger.json');
     let options = {};
+
     console.log(`App listening port is ${app.listeningPort}`);
     if(!process.env.hasOwnProperty('randomWorkerPort') || process.env.randomWorkerPort === false) {
       options.port = app.listeningPort;
@@ -15,6 +17,7 @@ const getSwagger = (app) => {
 
     let swaggerService = new SwaggerService('/api/v1', baseSwagger, options);
     swaggerService.getSwagger().then((swagger) => {
+      console.log(swagger);
       res.status(HttpStatus.OK).send(swagger);
     }).catch((err) => {
       new ServiceError(HttpStatus.INTERNAL_SERVER_ERROR, err.message).writeResponse(res);
